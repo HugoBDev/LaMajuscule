ff546d291201afe5bb086c1b21521e647d69661e
HugoBDev <hugobrd.pro@gmail.com>
2024-09-04 14:08:06 +0200

    added new components

---
+++ src/app/components/ambassador-card/ambassador-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ambassador-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './ambassador-card.component.html',
+  styleUrl: './ambassador-card.component.scss'
+})
+export class AmbassadorCardComponent {
+
+}
---
+++ src/app/components/ambassador/ambassador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ambassador',
+  standalone: true,
+  imports: [],
+  templateUrl: './ambassador.component.html',
+  styleUrl: './ambassador.component.scss'
+})
+export class AmbassadorComponent {
+
+}
