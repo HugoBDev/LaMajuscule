da4b8af63e401588b267898ac9ef4428a00632f4
HugoBDev <hugobrd.pro@gmail.com>
2024-09-06 12:48:42 +0200

    somes fixes

--- src/app/components/ambassador/ambassador.component.ts
+++ src/app/components/ambassador/ambassador.component.ts
@@ -11,52 +11,56 @@ export interface Ambassador {
 @Component({
   selector: 'app-ambassador',
   standalone: true,
-  imports: [CarouselModule, ButtonModule,],
+  imports: [CarouselModule, ButtonModule],
   templateUrl: './ambassador.component.html',
-  styleUrl: './ambassador.component.scss'
+  styleUrl: './ambassador.component.scss',
 })
 export class AmbassadorComponent {
-  ambassadors! : Ambassador[]
+  ambassadors!: Ambassador[];
 
   responsiveOptions: any[] | undefined;
 
   constructor() {}
 
   ngOnInit() {
-   
-    this.ambassadors  = [
+    this.ambassadors = [
       {
         id: '1',
-       image: 'https://images.unsplash.com/photo-1725034246126-a73525408934?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image:
+          'https://images.unsplash.com/photo-1725034246126-a73525408934?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'Hugo',
       },
       {
         id: '2',
-       image: 'https://images.unsplash.com/photo-1725462566776-64eb9548e0bc?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image:
+          'https://images.unsplash.com/photo-1725462566776-64eb9548e0bc?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'margot',
       },
       {
         id: '3',
-       image: 'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image:
+          'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'Ulysse',
       },
       {
         id: '4',
-       image: 'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image:
+          'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'Ulysse',
       },
       {
         id: '5',
-       image: 'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image:
+          'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'Ulysse',
       },
       {
         id: '6',
-       image: 'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image:
+          'https://images.unsplash.com/photo-1725429967564-cebd8d7a98d5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'Ulysse',
       },
-    ]
-
+    ];
 
     this.responsiveOptions = [
       {
