bd35c5069fab31a455d3b7721fda11425bdb533e
HugoBDev <hugobrd.pro@gmail.com>
2024-09-06 11:13:31 +0200

    added new footerComponent, make it responsive, still medias to add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { ClientAdviceComponent } from "./components/client-advice/client-advice.
 
 import { AmbassadorComponent } from './components/ambassador/ambassador.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +23,8 @@ import { PresentationComponent } from './components/presentation/presentation.co
     BenefitsComponent,
     ClientAdviceComponent,
     AmbassadorComponent,
-    PresentationComponent
+    PresentationComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
